41e99bb39d7b29bd1334a2e66294d640b3f41199
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-05-05 14:53:23 +0530

    Demo project for password encoder

---
+++ SpringBootSection4PasswordEncoderDemo/SpringBootSection4PasswordEncoderDemo/src/main/java/com/easyBank/passwordEncoderDemo/SpringBootSection4PasswordEncoderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.easyBank.passwordEncoderDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSection4PasswordEncoderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSection4PasswordEncoderDemoApplication.class, args);
+	}
+
+}
---
+++ SpringBootSection4PasswordEncoderDemo/SpringBootSection4PasswordEncoderDemo/src/test/java/com/easyBank/passwordEncoderDemo/SpringBootSection4PasswordEncoderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyBank.passwordEncoderDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSection4PasswordEncoderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
