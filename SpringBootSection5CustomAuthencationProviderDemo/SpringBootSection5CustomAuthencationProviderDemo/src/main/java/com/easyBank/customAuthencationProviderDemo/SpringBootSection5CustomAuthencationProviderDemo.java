07c1f10bd8923e1a61bd53a86688eff775e8be08
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-05-05 16:33:06 +0530

    1. Learn About Encoding, Encryption and Hashing. 2. Learn About Implementation of password encoding. 3. Add a new project for custom Authentication provider implementation demo.

---
+++ SpringBootSection5CustomAuthencationProviderDemo/SpringBootSection5CustomAuthencationProviderDemo/src/main/java/com/easyBank/customAuthencationProviderDemo/SpringBootSection5CustomAuthencationProviderDemo.java
@@ -0,0 +1,13 @@
+package com.easyBank.customAuthencationProviderDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSection5CustomAuthencationProviderDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSection5CustomAuthencationProviderDemo.class, args);
+	}
+
+}
---
+++ SpringBootSection5CustomAuthencationProviderDemo/SpringBootSection5CustomAuthencationProviderDemo/src/main/java/com/easyBank/customAuthencationProviderDemo/controller/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.easyBank.customAuthencationProviderDemo.controller.repository;
+
+import com.easyBank.passwordEncoderDemo.model.Users;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<Users, Serializable> {
+    List<Users> findByEmail(String username);
+}
